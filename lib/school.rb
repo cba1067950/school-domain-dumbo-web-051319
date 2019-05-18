e6b547821070c593bbe7fddc91f92e04789c2992
Joe Jamsky <cba1067950@yahoo.com>
2019-05-18 21:40:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   end
   
   def add_student(student, grade)
-    if roster[grade] 
+    if roster[grade] ||= 
       roster[grade] << student
     else 
       roster[grade] = []
@@ -25,11 +25,20 @@ class School
     #@roster.sort by name 
     newHash = {}
     @roster.each do |class_studends, students| 
+<<<<<<< HEAD
       newHash[class_studends] = []
       array = students.sort
       newHash[class_studends] += array
     end
     newHash
+=======
+      binding.pry
+      newHash[class_studends] 
+      newHash[class_studends] << students.sort
+      binding.pry 
+    end
+    binding.pry
+>>>>>>> 43dd6736a252f3e1001ef8831c3fec3c4c30d3cc
   end
 end 
 
